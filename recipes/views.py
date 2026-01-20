b01b4c7b33e94f39cfffd25de358b2b02f521610
abouhamidsaida9-hue <abouh@DELL2025.(none)>
2026-01-20 17:04:54 +0100

    Ignore pycache and database

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.contrib.auth.models import User
+from django.db.models import Q
 
-# Create your views here.
+def search_recipes(request):
+    query = request.GET.get('q')
+    author_id = request.GET.get('author')
+
+    recipes = Recipe.objects.all()
+    authors = User.objects.all()
+
+    if query:
+        recipes = recipes.filter(
+            Q(title__icontains=query) |
+            Q(ingredients__icontains=query)
+        )
+
+    if author_id:
+        recipes = recipes.filter(author__id=author_id)
+
+    context = {
+        'recipes': recipes,
+        'authors': authors,
+        'query': query,
+        'author_id': author_id,
+    }
+    return render(request, 'recipes/search.html', context)
